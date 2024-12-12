1fa86f7a180a7bb289e374401097df2d83651a90
Guillaume Daguet <g.daguet@ateme.com>
2024-12-12 17:24:40 +0100

    Add update ott config script

--- create_basic_ott_conf.py
+++ create_basic_ott_conf.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
 import argparse
 from SoapClient import SoapClient
-import importlib.util
-
-MODULE_PATH = "/home/gdaguet/src/Workspace/Repos/soap-common/SoapClient.py"
-MODULE_NAME = "SoapClient"
-
-spec = importlib.util.spec_from_file_location(MODULE_NAME, MODULE_PATH)
-modulevar = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(modulevar)
 
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("host")
 parser.add_argument("-c", "--clean", action="store_true", required=False)
+
+# define optional option to select the stream between arte_tl, arte, france2_tl, tv5, tv5_tl, if no option value equal to all streams
+parser.add_argument(
+    "-s",
+    "--stream",
+    choices=["arte_tl", "arte", "france2_tl", "tv5", "tv5_tl"],
+    required=False,
+)
+
+# parser.add_argument(
+#     "-s",
+#     "--stream",
+#     action="store_true",
+#     choices=["arte_tl", "arte", "france2_tl", "tv5", "tv5_tl"],
+# )
+
+parser.add_argument("-n", "--nb_channel", type=int, default=1, required=False)
+
 
 args = parser.parse_args()
 print(args)
@@ -34,116 +44,173 @@ disk = "disk1"
 _type = "ts-generic"
 
 SAF = "saf"
-CHANNEL_NAME_PREFIXE = "generic_channel_"
-NB_CHANNEL = 1
 
 # soap.Set_ott_conf({"chunkDurations": ["2"]})
 
-# # Stream
-# ip = "239.10.0.1"
+streams = {}
 
-# sources_ip = [
-#     "udp://%s:20000:%s" % (ip, interface),
-#     "udp://%s:20001:%s" % (ip, interface),
-#     "udp://%s:20002:%s" % (ip, interface),
-#     "udp://%s:20003:%s" % (ip, interface),
-#     "udp://%s:20004:%s" % (ip, interface),
-#     "udp://%s:20005:%s" % (ip, interface),
-#     "udp://%s:20010:%s" % (ip, interface),
-#     "udp://%s:20011:%s" % (ip, interface),
-#     "udp://%s:20012:%s" % (ip, interface),
-# ]
 
-# Stream
-ip = "239.2.2.4"
-
-sources_ip = [
-    "udp://%s:1234:%s" % (ip, interface),
-    "udp://%s:1235:%s" % (ip, interface),
-    "udp://%s:1236:%s" % (ip, interface),
-    "udp://%s:1237:%s" % (ip, interface),
-    "udp://%s:1238:%s" % (ip, interface),
-    "udp://%s:1239:%s" % (ip, interface),
-    "udp://%s:1240:%s" % (ip, interface),
-    "udp://%s:1241:%s" % (ip, interface),
-    "udp://%s:1242:%s" % (ip, interface),
-    "udp://%s:1243:%s" % (ip, interface),
-    "udp://%s:1244:%s" % (ip, interface),
+# Stream Arte
+ip_arte = "239.68.0.11"
+streams["arte_tl"] = [
+    "udp://%s:1234:%s" % (ip_arte, interface),
+    "udp://%s:1235:%s" % (ip_arte, interface),
+    "udp://%s:1236:%s" % (ip_arte, interface),
+    "udp://%s:1237:%s" % (ip_arte, interface),
 ]
 
-# ip = "239.10.1.1"
-# sources_ip = ["udp://%s:5009:%s" % (ip, interface)]
+# Stream Arte (rebouclage)
+ip_arte_loop = "239.68.0.100"
+streams["arte"] = [
+    "udp://%s:1234:%s" % (ip_arte_loop, interface),
+    "udp://%s:1235:%s" % (ip_arte_loop, interface),
+    "udp://%s:1236:%s" % (ip_arte_loop, interface),
+    "udp://%s:1237:%s" % (ip_arte_loop, interface),
+    "udp://%s:1238:%s" % (ip_arte_loop, interface),
+    "udp://%s:1239:%s" % (ip_arte_loop, interface),
+    "udp://%s:1240:%s" % (ip_arte_loop, interface),
+    "udp://%s:1241:%s" % (ip_arte_loop, interface),
+    "udp://%s:1242:%s" % (ip_arte_loop, interface),
+    "udp://%s:1243:%s" % (ip_arte_loop, interface),
+    "udp://%s:1244:%s" % (ip_arte_loop, interface),
+]
 
-if args.clean:
-    # Cleaning
-    print("Removing channels...")
-    listChannel = soap.List_live_channels()
-    # while len(listChannel) != 0:
-    for c in listChannel:
-        print("    ", f"Delete {c}")
-        try:
-            soap.Delete_live_channel(c)
-        except Exception as e:
-            print(f"Error during deleting {c}")
-        # listChannel = soap.List_live_channels()
+# Stream France2
+ip_france2 = "239.68.0.12"
+streams["france2_tl"] = [
+    "udp://%s:1234:%s" % (ip_france2, interface),
+    "udp://%s:1235:%s" % (ip_france2, interface),
+    "udp://%s:1236:%s" % (ip_france2, interface),
+]
 
-    print("Removing CPIX server...")
-    latest_server_to_delete = None
-    for server_name in soap.List_scrambling_servers():
-        props = soap.Get_scrambling_server_props(server_name)
-        if props["conf"]["isDefault"]:
-            latest_server_to_delete = server_name
-        else:
-            soap.Delete_scrambling_server_conf(server_name)
-    if latest_server_to_delete:
-        soap.Delete_scrambling_server_conf(latest_server_to_delete)
+# Stream TV5 Monde
+ip_tv5_monde = "239.68.0.13"
+streams["tv5_tl"] = [
+    "udp://%s:1234:%s" % (ip_tv5_monde, interface),
+    "udp://%s:1235:%s" % (ip_tv5_monde, interface),
+    "udp://%s:1236:%s" % (ip_tv5_monde, interface),
+    "udp://%s:1237:%s" % (ip_tv5_monde, interface),
+    "udp://%s:1238:%s" % (ip_tv5_monde, interface),
+    "udp://%s:1239:%s" % (ip_tv5_monde, interface),
+]
 
-    print("Removing SA/SAF...")
-    for saf in soap.List_stream_adaptation_families():
-        soap.Delete_stream_adaptation_family(saf)
-    for sa in soap.List_stream_adaptations():
-        soap.Delete_stream_adaptation(sa)
+# Stream TV5 Monde
+ip_tv5_monde_loop = "239.68.0.101"
+streams["tv5"] = ["udp://%s:1234:%s" % (ip_tv5_monde_loop, interface)]
 
 
-if not args.clean:
-    # SA
-    print("Creating SA/SAF...")
-    sa_name = "sa_dash"
-    conf = {
-        "outputFormat": "dash",
-        "chunkDuration": 10,
-        "subtitlesFormat": "ttmlpassthrough",
-    }
-    soap.Set_stream_adaptation_conf(sa_name, conf)
+def main():
 
-    sa_name = "sa_hls"
-    conf = {
-        "outputFormat": "hls",
-        "chunkDuration": 10,
-        "subtitlesFormat": "webvtt",
-    }
-    soap.Set_stream_adaptation_conf(sa_name, conf)
+    if args.clean:
+        # Cleaning
+        print("Removing channels...")
+        listChannel = soap.List_live_channels()
+        # while len(listChannel) != 0:
+        for c in listChannel:
+            print("    ", f"Delete {c}")
+            try:
+                soap.Delete_live_channel(c)
+            except Exception as e:
+                print(f"Error during deleting {c}")
+            # listChannel = soap.List_live_channels()
 
-    # SAF
-    soap.Set_stream_adaptation_family_conf(
-        SAF,
-        {
-            "streamAdaptations": [
-                "sa_dash",
-                "sa_hls",
-            ]
-        },
-    )
+        print("Removing CPIX server...")
+        latest_server_to_delete = None
+        for server_name in soap.List_scrambling_servers():
+            props = soap.Get_scrambling_server_props(server_name)
+            if props["conf"]["isDefault"]:
+                latest_server_to_delete = server_name
+            else:
+                soap.Delete_scrambling_server_conf(server_name)
+        if latest_server_to_delete:
+            soap.Delete_scrambling_server_conf(latest_server_to_delete)
 
-    # Channels
+        print("Removing SA/SAF...")
+        for saf in soap.List_stream_adaptation_families():
+            soap.Delete_stream_adaptation_family(saf)
+        for sa in soap.List_stream_adaptations():
+            soap.Delete_stream_adaptation(sa)
 
-    channel_options = {
-        "streamAdaptationFamily": SAF,
-        "sourceUris": sources_ip,
-    }
+    if not args.clean:
 
-    print("Creating channels...")
-    for i in range(0, NB_CHANNEL):
-        ch_name = f"{CHANNEL_NAME_PREFIXE}{i:03}"
-        soap.Create_live_channel(ch_name, disk, _type, channel_options)
-        print("    ", f"Add {ch_name}")
+        streams_selected = streams
+        if args.stream:
+            streams_to_delete = [stream for stream in streams if stream != args.stream]
+            for stream in streams_to_delete:
+                del streams_selected[stream]
+
+        # SA
+        print("Creating SA/SAF...")
+        sa_name = "sa_dash"
+        conf = {
+            "outputFormat": "dash",
+            "chunkDuration": 10,
+            "subtitlesFormat": "ttmlpassthrough",
+        }
+        soap.Set_stream_adaptation_conf(sa_name, conf)
+
+        sa_name = "sa_hls"
+        conf = {
+            "outputFormat": "hls",
+            "chunkDuration": 10,
+            "subtitlesFormat": "webvtt",
+        }
+        soap.Set_stream_adaptation_conf(sa_name, conf)
+
+        sa_name = "sa_cmaf"
+        conf = {
+            "outputFormat": "cmaf",
+            "chunkDuration": 10,
+            "subtitlesFormat": "webvtt",
+        }
+        soap.Set_stream_adaptation_conf(sa_name, conf)
+
+        # SAF
+        soap.Set_stream_adaptation_family_conf(
+            SAF,
+            {
+                "streamAdaptations": [
+                    "sa_dash",
+                    "sa_hls",
+                    "sa_cmaf",
+                ]
+            },
+        )
+
+        # Channels
+        print("Creating channels...")
+        for channel_name, sources_ip in streams.items():
+
+            channel_options = {
+                "streamAdaptationFamily": SAF,
+                "mbtsSources": [
+                    {
+                        "id": "main",
+                        "enable": True,
+                        "udp_sources": sources_ip,
+                        "group": "default",
+                        "multicast_origins": None,
+                    }
+                ],
+                "archiveLifecycle": [
+                    {
+                        "action": "delete-unused-data",
+                        "disk": "disk1",
+                        "durationOffset": 600,
+                    },
+                    {
+                        "action": "delete-all-data",
+                        "disk": "disk1",
+                        "durationOffset": 600,
+                    },
+                ],
+            }
+
+            for i in range(0, args.nb_channel):
+                ch_name = f"{channel_name}_{i:03}"
+                soap.Create_live_channel(ch_name, disk, _type, channel_options)
+                print("    ", f"Add {ch_name}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ update_config_ott.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+import os
+from urllib.parse import urlparse
+import argparse
+import subprocess
+import datetime
+import sys
+import json
+import logging
+import SoapClient
+
+logger = logging.getLogger()
+
+HOST = "https://dvr1.gen10.lab1.anevia.com:8443"
+# HOST = "https://nea-live-ref.lab1.anevia.com:8443"
+SUT_USER = "admin"
+SUT_PWD = "paris"
+SOURCE_URI_PREFIXE = "udp://239.68"
+
+
+# Update the sourceUri of all live channels
+def update_sourceUri(soap_client, channel_prop):
+
+    mbtsSources = channel_prop["conf"]["options"]["mbtsSources"]
+    udp_sources = mbtsSources[0]["udp_sources"]
+    new_udp_sources = []
+    for udp_source in udp_sources:
+
+        # Get the sourceUris index
+        tmp = udp_source.split("//")
+        prefix_udp = tmp[0]
+        url_full = tmp[1]
+        ip, port, lan = url_full.split(":")
+        ip1, ip2, ip3, ip4 = ip.split(".")
+
+        # newSourceUriEDS = f"{SOURCE_URI_PREFIXE}.1{ip4}.0:{port}:{lan}"
+        # print(f"Updating sourceUri from {sourceUri} to {newSourceUriEDS}")
+
+        new_udp_source = f"{SOURCE_URI_PREFIXE}.{ip4}.0:{port}:{lan}"
+        print(f"Updating sourceUri from {udp_source} to {new_udp_source}")
+
+        new_udp_sources.append(new_udp_source)
+
+    # Update the sourceUri
+    mbtsSources[0]["udp_sources"] = new_udp_sources
+
+    option = {"mbtsSources": mbtsSources}
+    print(f"Updating udp_sources from {udp_sources} to {new_udp_sources}")
+
+    return option
+
+
+def main():
+    # Initialize an ASOAP object
+    sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+    logger.info(f"Connecting to {HOST}")
+    soap_client = SoapClient.SoapClient(HOST, "ott", (SUT_USER, SUT_PWD))
+
+    # Update channel sourceUris from 239.10.10.1 to 239.68.1.1 by using self.soap_client.Modify_live_channel() api
+
+    # Get all live channels
+    live_channels = soap_client.List_live_channels()
+    for live_channel in live_channels:
+        print(live_channel)
+        # Get the sourceUris
+        channel_props = soap_client.Get_live_channel_props(live_channel)
+
+        print(channel_props)
+        # channel_props_updated = update_sourceUri(soap_client, channel_props)
+
+        # Update the channel
+        # soap_client.Modify_live_channel(live_channel, channel_props_updated)
+
+
+if __name__ == "__main__":
+    main()
